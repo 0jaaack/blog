3ec537f01b6ae91d9e4550167a73aad16f2579b2
Jack <jack@hopsoffice.com>
2024-04-24 13:19:29 +0900

    add theme icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
+import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jaaack.dev",
   integrations: [mdx(), sitemap(), preact()],
+  vite: {
+    plugins: [svgr()],
+  },
 });
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "공재혁 블로그";
+export const DEFAULT_THEME: "light" | "dark" = "dark";
