b8f4a1e0b6b74828020110f0eff85f40fc9ab51f
Jack <jack@hopsoffice.com>
2024-04-24 09:14:37 +0900

    add preact to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jaaack.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), preact()],
 });
