89d5cea329df353cbcbe745da17e2eca01aeabda
Jack <jack@hopsoffice.com>
2024-05-19 22:18:22 +0900

    각 포스트 내용이 OG 이미지에 표시되도록 수정

---
+++ src/utils/createImageFromPost.ts
@@ -0,0 +1,32 @@
+import fs from "fs/promises";
+import type { JSX } from "preact/jsx-runtime";
+import satori from "satori";
+import sharp from "sharp";
+import { PostOpenGraphImage } from "../components/PostOpenGraphImage";
+import type { Essay } from "../pages/[slug]/[slug]-og.png";
+
+export async function getSVGImageFromComponent(component: JSX.Element) {
+  return await satori(component, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "Pretendard",
+        data: await fs.readFile("./src/assets/fonts/Pretendard-Regular.ttf"),
+        weight: 400,
+      },
+      {
+        name: "Pretendard",
+        data: await fs.readFile("./src/assets/fonts/Pretendard-Bold.ttf"),
+        weight: 700,
+      },
+    ],
+  });
+}
+
+export const getPNGImageFromPost = async (essay: Essay): Promise<Buffer> => {
+  const component = PostOpenGraphImage(essay);
+  const SVG = await getSVGImageFromComponent(component);
+
+  return await sharp(Buffer.from(SVG)).png().toBuffer();
+};
